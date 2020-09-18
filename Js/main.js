5323e498035a106a028275f6a3ea652858efed51
Gabriel Torres <gtorress106@gmail.com>
2020-09-18 12:36:57 -0600

    Finalizacion del proyecto
    Se finaliza el html y css con la adaptabilidad a dispositivos moviles.
    Se agrega animaciones con Js

---
+++ Js/main.js
@@ -0,0 +1,28 @@
+/*Aparece y desaparece menu segun scroll*/
+let ubicacionPrincipal = window.pageYOffset;
+
+window.addEventListener("scroll", function () {
+    let desplazamientoActual = window.pageYOffset;
+    if (ubicacionPrincipal >= desplazamientoActual) {
+        document.getElementsByTagName("nav")[0].style.top = "0px";
+    } else {
+        document.getElementsByTagName("nav")[0].style.top = "-100px";
+    }
+    ubicacionPrincipal = desplazamientoActual;
+});
+
+/*Mostrat menu hamburguesa*/
+
+let enlacesHeader = document.querySelectorAll(".enlaces-header")[0];
+let semaforo = true;
+
+document.querySelectorAll(".hamburger")[0].addEventListener("click", function () {
+    if (semaforo) {
+        document.querySelectorAll(".hamburger")[0].style.color = "#fff";
+        semaforo = false;
+    } else {
+        document.querySelectorAll(".hamburger")[0].style.color = "#000";
+        semaforo = true;
+    }
+    enlacesHeader.classList.toggle("menudos");
+});
